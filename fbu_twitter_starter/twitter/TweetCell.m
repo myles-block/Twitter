34d82e92f237033771ae9b93989b5150b657fb02
myles-block <myles.block03@gmail.com>
2022-06-24 02:19:02 -0700

    Added DateTools libraries and started data refresh for retweet/like

--- fbu_twitter_starter/twitter/TweetCell.m
+++ fbu_twitter_starter/twitter/TweetCell.m
@@ -20,5 +20,30 @@
 
     // Configure the view for the selected state
 }
+- (IBAction)didTapFavorite:(id)sender {
+    // TODO: Update the local tweet model
+    self.tweet.favorited = YES;
+    self.tweet.favoriteCount += 1;
+    
+    
+    //MARK: work on updating cell UI
+//    // TODO: Update cell UI
+//    self.userNameLabel.text = self.tweet.user.name;
+//    self.userBodyLabel.text = self.tweet.text;
+//    self.screenNameLabel.text = self.tweet.user.screenName;
+//    self.createdAtLabel.text = self.tweet.createdAtString;
+//
+//    NSString* retweetString = [[NSString alloc] initWithFormat:@"%d", self.tweet.retweetCount];
+//
+//    NSString* likeString = [[NSString alloc] initWithFormat:@"%d", self.tweet.favoriteCount];
+    
+    
+    
+    // TODO: Send a POST request to the POST favorites/create endpoint
+}
+
+
+
+
 
 @end
